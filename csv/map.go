90bd3c1149dc48de1ba4156a80fe700c86b60515
Peter <peeter.kokk@gmail.com>
2018-10-21 12:27:18 +0300

    add synchronized map

--- csv/map.go
+++ csv/map.go
@@ -1,47 +1,27 @@
 package csv
 
-import (
-	"encoding/csv"
-	"io"
-	"os"
-)
+import "sync"
 
-type fileData struct {
-	reader  *csv.Reader
-	csvFile *os.File
+type SynchronizedMap struct {
+	sync.RWMutex
+	unsynchronizedMap map[string][2]string
 }
 
-func newFileData(filepath string) (*fileData, error) {
-	csvfile, err := os.Open(filepath)
-
-	if err != nil {
-		return nil, err
+func NewSynchronizedMap() *SynchronizedMap {
+	return &SynchronizedMap{
+		unsynchronizedMap: make(map[string][2]string),
 	}
-
-	reader := csv.NewReader(csvfile)
-
-	return &fileData{
-		reader:  reader,
-		csvFile: csvfile,
-	}, nil
 }
 
-func MapAllFromFilepath(filepath string) (records map[string][2]string, err error) {
-	fileData, err := newFileData(filepath)
-	if err != nil {
-		return nil, err
-	}
-	defer fileData.csvFile.Close()
+func (sm *SynchronizedMap) Load(key string) (value [2]string, ok bool) {
+	sm.RLock()
+	result, ok := sm.unsynchronizedMap[key]
+	sm.RUnlock()
+	return result, ok
+}
 
-	records = make(map[string][2]string)
-	var record []string
-
-	for record, err = fileData.reader.Read(); err == nil; record, err = fileData.reader.Read() {
-		records[record[0]] = [2]string{record[1], record[2]}
-	}
-
-	if err != nil && err != io.EOF {
-		return nil, err
-	}
-	return records, nil
+func (sm *SynchronizedMap) UpdateMap(newMap map[string][2]string) {
+	sm.Lock()
+	sm.unsynchronizedMap = newMap
+	sm.Unlock()
 }
---
+++ csv/records.go
@@ -0,0 +1,47 @@
+package csv
+
+import (
+	"encoding/csv"
+	"io"
+	"os"
+)
+
+type fileReader struct {
+	reader  *csv.Reader
+	csvFile *os.File
+}
+
+func newfileReader(filepath string) (*fileReader, error) {
+	csvfile, err := os.Open(filepath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	reader := csv.NewReader(csvfile)
+
+	return &fileReader{
+		reader:  reader,
+		csvFile: csvfile,
+	}, nil
+}
+
+func GetRecordsFromCsvFile(filepath string) (records map[string][2]string, err error) {
+	fileReader, err := newfileReader(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer fileReader.csvFile.Close()
+
+	records = make(map[string][2]string)
+	var record []string
+
+	for record, err = fileReader.reader.Read(); err == nil; record, err = fileReader.reader.Read() {
+		records[record[0]] = [2]string{record[1], record[2]}
+	}
+
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	return records, nil
+}
