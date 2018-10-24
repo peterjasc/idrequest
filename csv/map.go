67d1a402a22e82a26d5de67bca00d75562e50be3
Peter <peeter.kokk@gmail.com>
2018-10-24 11:54:05 +0300

    pass map as pointer not value

--- csv/map.go
+++ csv/map.go
@@ -4,23 +4,23 @@ import "sync"
 
 type SynchronizedMap struct {
 	sync.RWMutex
-	unsynchronizedMap map[string][2]string
+	unsynchronizedMap *map[string][2]string
 }
 
 func NewSynchronizedMap() *SynchronizedMap {
 	return &SynchronizedMap{
-		unsynchronizedMap: make(map[string][2]string),
+		unsynchronizedMap: nil,
 	}
 }
 
 func (sm *SynchronizedMap) Load(key string) (result [2]string, ok bool) {
 	sm.RLock()
-	result, ok = sm.unsynchronizedMap[key]
+	result, ok = (*sm.unsynchronizedMap)[key]
 	sm.RUnlock()
 	return result, ok
 }
 
-func (sm *SynchronizedMap) UpdateMap(newMap map[string][2]string) {
+func (sm *SynchronizedMap) UpdateMap(newMap *map[string][2]string) {
 	sm.Lock()
 	sm.unsynchronizedMap = newMap
 	sm.Unlock()
--- csv/reader.go
+++ csv/reader.go
@@ -26,14 +26,14 @@ func newfileReader(filepath string) (*fileReader, error) {
 	}, nil
 }
 
-func GetRecordsFromCsvFile(filepath string) (records map[string][2]string, err error) {
+func GetRecordsFromCsvFile(filepath string) (*map[string][2]string, error) {
 	fileReader, err := newfileReader(filepath)
 	if err != nil {
 		return nil, err
 	}
 	defer fileReader.csvFile.Close()
 
-	records = make(map[string][2]string)
+	records := make(map[string][2]string)
 	var record []string
 	for record, err = fileReader.reader.Read(); err == nil; record, err = fileReader.reader.Read() {
 		records[record[0]] = [2]string{record[1], record[2]}
@@ -42,5 +42,5 @@ func GetRecordsFromCsvFile(filepath string) (records map[string][2]string, err e
 	if err != nil && err != io.EOF {
 		return nil, err
 	}
-	return records, nil
+	return &records, nil
 }
